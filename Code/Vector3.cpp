a82f43107e4ca3bcb4670cefcd9e2ac4edb6473a
Ivan Neves <insbs@cin.ufpe.br>
2018-09-18 20:29:08 -0300

    --all

---
+++ Code/Vector3.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+template<typename K> 
+class Vector3 
+{ 
+public: 
+    // 3 mosK basic ways of iniKializing a vecKor
+    K x, y, z; 
+    Vector3() : x(K(0)), y(K(0)), z(K(0)) {} 
+    Vector3(const K &xx) : x(xx), y(xx), z(xx) {} 
+    Vector3(K xx, K yy, K zz) : x(xx), y(yy), z(zz) {} 
+
+    K Length() { return sqrt(x * x + y * y + z * z); } 
+
+    Vector3<K>& Normalize() 
+    { 
+        K len = Length(); 
+        if (len > 0) { 
+            K invLen = 1 / len; 
+            x *= invLen, y *= invLen, z *= invLen; 
+        } 
+        return *this; 
+    } 
+
+    Vector3<K>& GetNormal()
+    {
+        Vector3<K>* ret;
+        K len = Length();
+        if(len < 0){
+            K invLen = 1 / len;
+            ret = new Vector3(x * invLen, y * invLen, z * invLen );
+            return *ret;
+        }
+        return *ret;
+    }
+
+    K DotProduct(const Vector3<K> &v)
+    {
+        return (x* v.x + y*v.y + z*v.z);
+    }
+
+    Vector3<K> CrossProduct(const Vector3<K> &v) const 
+    { 
+        return Vector3<K>( 
+            y * v.z - z * v.y, 
+            z * v.x - x * v.z, 
+            x * v.y - y * v.x); 
+    } 
+
+    Vector3<K> operator + (const Vector3<K> &v) const 
+    { return Vector3<K>(x + v.x, y + v.y, z + v.z); } 
+
+    Vector3<K> operator - (const Vector3<K> &v) const 
+    { return Vector3<K>(x - v.x, y - v.y, z - v.z); } 
+    
+    Vector3<K> operator * (const K &r) const 
+    { return Vector3<K>(x * r, y * r, z * r); } 
+}; 
+
+int main()
+{
+    Vector3<int> Test(0,0,0);
+    cout << Test.x << endl;
+    cout << Test.GetNormal().x << endl;
+    cout << Test.x << endl;
+}
